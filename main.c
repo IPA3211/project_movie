8e1f0b0aeeccba695282d51bfff0b390810e2385
IPA3211 <kygsm2003@naver.com>
2017-11-21 19:35:32 +0900

    get comment 만드는중

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "seungmo.h"
 #include "jeawoo.h"
 #include "cheolsoon.h"
 #include "junwon.h"
 
 int main(void){
-	print();
+	char *a = (char *)malloc(sizeof(char));
+
+ 	put_commend(a);
+    print(a);
+
+	printf("\n");
 	return 0;
 }
--- seungmo.h
+++ seungmo.h
@@ -1,7 +1,18 @@
 // seung mo
 #include <stdio.h>
-
-int print(void){
-	printf("asd\n");
+#include <string.h>
+int print(char *test){
+	printf("test = %s\n", test);
 	return 0;
+}
+
+void put_commend(char *a){
+    printf("(movie) ");
+	scanf("%[^\n]s", a);
+    
+    char *tok = strtok(a," ");
+    char *commned_name = tok;
+    
+    if (strcmp(*tok, "search"))
+    print(commned_name);
 }
